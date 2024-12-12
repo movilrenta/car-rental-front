2d095cdb3b56e9c1ac3cf424862b316573a8b697
Fernando Arroyo <fer3443@gmail.com>
2024-12-11 21:31:49 -0300

    feat: formulario de pago terminado, cambios en formulario de confirmacion de reserva

--- actions/index.ts
+++ actions/index.ts
@@ -1,2 +1,3 @@
 export * from './get-token-pay';
 export * from './save-card';
+export * from './get-payment-methods';
--- components/schemas/reservas.ts
+++ components/schemas/reservas.ts
@@ -1,11 +1,10 @@
 import { z } from "zod";
 
 export const reservasSchema = z.object({
-    name: z.string().min(2,{message:"El nombre es obligatorio"}),
+    name: z.string().min(2,{message:"El nombre es obligatorio"}).max(30, "Nombre demasiado extenso"),
+    lastName: z.string().min(2,"El apellido debe contener al menos 2 caracteres").max(25,"No se admiten más de 25 caracteres"),
     email:z.string().email({message:"El correo es obligatorio"}),
     phone:z.string().min(10,{message:"El telefono es obligatorio y debe contener como minimo 10 caracteres"}),
-    company:z.string().optional(),
-    coments:z.string().max(500).optional(),
     termyCond: z.boolean().default(false),
     mayor25: z.boolean().default(false)
   })
