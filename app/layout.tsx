2e78943297509636cfb378f3e9ad2d1056e8fe30
movilrenta <javier@movilrenta.com.ar>
2024-12-23 10:45:56 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - | Movil Renta",
+    template: "%s | Movil Renta",
     default: "Movil Renta",
   },
   icons: {
