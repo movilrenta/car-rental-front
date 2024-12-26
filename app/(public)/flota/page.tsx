c021a2d5b683963dd6a8598c9509a2882a71d85e
Fernando Arroyo <fer3443@gmail.com>
2024-12-26 14:45:58 -0300

    added:flota de vehiculos view

--- app/(public)/flota/page.tsx
+++ app/(public)/flota/page.tsx
@@ -1,7 +1,18 @@
+import { GetCarsAction } from "@/actions/car";
 import { Flota } from "@/view";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
-export default function FlotaPage() {
+export const metadata:Metadata = {
+  title:"Flota de vehículos | Movil Renta"
+}
+
+export default async function FlotaPage() {
+  const resp = await GetCarsAction()
+  if(!resp){
+    redirect('/home')
+  }
   return (
-   <Flota/>
+   <Flota cars={resp}/>
   );
 }
