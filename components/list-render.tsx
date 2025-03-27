77a13d3f0289f5f08d84764c4c77278308158a98
Fernando Arroyo <fer3443@gmail.com>
2025-03-27 11:38:42 -0300

    add:TyC view

--- components/list-render.tsx
+++ components/list-render.tsx
@@ -8,14 +8,14 @@ interface ListItem {
 
 export const ListRender: React.FC<{ items: ListItem[] }> = ({ items }) => {
   return (
-    <ol className="flex flex-col gap-2 p-4">
+    <ol className="flex flex-col gap-2 p-4 max-w-7xl mx-auto">
       {items.map((item, index) => (
-        <li key={index} className="flex flex-col gap-2 md:gap-4">
-          <h3 className="font-semibold text-lg"><span className="mr-2">{index + 1}.</span>{item.title}</h3>
-          {item.content && <p>{item.content}</p>}
+        <li key={index} className="flex flex-col pb-2">
+          <h3 className="font-semibold text-lg"><span className="mr-1">{index + 1}.</span>{item.title}</h3>
+          {item.content && <p className="md:ps-4">{item.content}</p>}
           {item.subItems &&
             item.subItems.map((subItem, i) => (
-              <ul key={i} className="list-disc list-inside p-2">
+              <ul key={i} className="list-disc list-inside md:ps-4">
                 <li>{subItem.title}</li>
                 {subItem.content && <p>{subItem.content}</p>}
               </ul>
